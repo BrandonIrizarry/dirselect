ac25ab478d2153fc83a97a376b15f845cb38ee7e
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2026-02-03 18:59:09 -0500

    fix: replace "4" with midpoint between viewMax and viewMin

--- dirselect.go
+++ dirselect.go
@@ -240,7 +240,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.lineNumber = 0
 			}
 
-			if m.viewMin > 0 && m.lineNumber < m.viewMin+4 {
+			if m.viewMin > 0 && m.lineNumber < (m.viewMax+m.viewMin)/2 {
 				m.viewMin--
 				m.viewMax--
 			}
